a2a2850fd10749ed3ecfaf25ecc342eef569cb10
sloanej <46733794+sloanej@users.noreply.github.com>
2019-04-23 20:57:28 -0400

    Update population.sql

--- population.sql
+++ population.sql
@@ -1,22 +1,21 @@
 -- inserting into applications
 INSERT INTO users VALUES 
   -- Systems Administrator
-  ("SA", "Dietrich", "Reidenbaugh", "123456", "dreidenbaugh@gwu.edu", 5),
+  ("SA", "Julia", "Bristow", "julia320", "admin1", "julia_bristow@gwu.edu", 12345678),
   -- Graduate Secretary
-  ("GS", "Dawn", "Ginetti", "123456", "dawn@gwu.edu", 6),
+  ("GS", "Jack", "Sloane", "jacksloane", "password", "email@gmail.com", 13254761),
   -- Faculty Reviewer
-  ("FR", "Bhagi", "Narahari", "123456", "narahari@gwu.edu", 10),
+  ("FR", "Bhagi", "Narahari", "bn", "password", "narahari@gwu.edu", 21147362),
   -- Chair of Admissions Comm
-  ("CAC", "John", "Smith" "123456", "jsmith@gmail.com", 42142172),
+  ("CAC", "John", "Smith", "jsmith", "123456", "jsmith@gmail.com", 42142172),
   -- Applicants
-  ("A", "John", "Lennon", "plsletmein", "john_lennon@gmail.com", 55555555),
-  ("A", "Ringo", "Starr", "Apply!", "ringostarr@gmail.com", 66666666);
-
+  ("A", "John", "Lennon", "john_lennon", "plsletmein", "john_lennon@gmail.com", 55555555),
+  ("A", "Ringo", "Starr", "rstarr", "Apply!", "ringostarr@gmail.com", 66666666);
 
 -- insert personal data for applicants
 INSERT INTO personal_info VALUES
-  ("John", "Lennon", 55555555, "123 Main St, New York NY", 111111111),
-  ("Ringo", "Starr", 66666666, NULL, 222111111);
+  ("John", "Lennon", 55555555, "123 Main St", "New York", "NY", "23321", "8604626594", 111111111),
+  ("Ringo", "Starr", 66666666, NULL, NULL, NULL, NULL, NULL, 222111111);
 
 -- John's application (complete)
 INSERT INTO rec_letter (fname, lname, email, institution, uid) VALUES ("Recommender", "1", "recommend@gmail.com", "GWU", 55555555);
@@ -26,8 +25,8 @@ INSERT INTO prior_degrees VALUES (3.6, 2017, "GWU", "Computer Science", 55555555
 
 INSERT INTO app_review (uid, reviewerRole) VALUES (66666666, "FR");
 INSERT INTO app_review (uid, reviewerRole) VALUES (66666666, "CAC");
-INSERT INTO app_review (uid, reviewerRole, status) VALUES (55555555, "FR", 5);
-INSERT INTO app_review (uid, reviewerRole, status) VALUES (55555555, "CAC", 5);
+INSERT INTO app_review (uid, reviewerRole, status) VALUES (55555555, "FR", 7);
+INSERT INTO app_review (uid, reviewerRole, status) VALUES (55555555, "CAC", 7);
 
 -- Ringo's application (incomplete)
 INSERT INTO academic_info (uid, transcript, recletter) VALUES (66666666, false, false);
