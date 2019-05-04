65b92a79b8fa8a2422175ab5749d4084c2605267
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-05-04 14:39:54 -0400

    styling

--- public_html/advising/form1.php
+++ public_html/advising/form1.php
@@ -56,12 +56,94 @@ $university_id = $_SESSION['uid'];
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<style>
+  /*tbody tr:nth-child(odd) {
+      background-color: #ff33cc;
+  }
+
+  tbody tr:nth-child(even) {
+      background-color: #e495e4;
+  }
+
+  h2 {
+    color: #5689DF;
+  }*/
+
+  .center{
+    text-align: center;
+  }
+
+  .topright {
+      position: absolute;
+      right: 10px;
+      top: 20px;
+    }
+    .btn {
+      background-color: #990000;
+      color: white;
+      padding: 12px;
+      margin: 10px 0;
+      border: none;
+      width: 40%;
+      border-radius: 3px;
+      cursor: pointer;
+      font-size: 17px;
+    }
+    .field {
+      position: absolute;
+      left: 180px;
+    }
+    /*body{line-height: 1.6;}*/
+    .bottomCentered{
+       position: fixed;
+       text-align: center;
+       bottom: 30px;
+       width: 100%;
+    }
+    .error {color: #FF0000;}
+    .topright {
+      position: absolute;
+      right: 10px;
+      top: 10px;
+    }
+
+    ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+    }
+
+    li {
+    float: left;
+    }
+
+    li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    }
+
+    li a:hover:not(.active) {
+    background-color: #111;
+    }
+
+    .active {
+      background-color: #990000;
+    }
+
+</style>
+
+<link rel="stylesheet" href="style.css">
 <head>
 <!--  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <link rel="stylesheet" type="text/css" href="style.css" />-->
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <style>
+  <!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
+  <!-- <style>
   ul {
   list-style-type: none;
   margin: 0;
@@ -91,7 +173,7 @@ li a:hover:not(.active) {
 .active {
   background-color: #4CAF50;
 }
-</style>
+</style> -->
 <ul>
   <li><a class="active" href="student.php">Advising Home</a></li>
   <!-- <li><a href="StudentEnrollmentInfo.php">Current Enrolment</a></li>
