1eb4a1bec7c2893db84346ab477e45da84cc076a
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-05-03 23:51:32 -0400

    view form1 hopefully fixed?

--- public_html/advising/viewform1.php
+++ public_html/advising/viewform1.php
@@ -111,7 +111,7 @@ background-color: #4CAF50;
   <?php
 
   if($credits_sum<30){
-    echo $credits_sum;
+    //echo $credits_sum;
     echo "<b>ERROR: You need at least 30 credits to graduate. Please fill out Form 1 with the necessary credentials.</b></br>";
     $sql_1 = "SELECT * FROM form1 WHERE university_id =" .$user.";";
     // $result = mysqli_query($db, $sql_1);
