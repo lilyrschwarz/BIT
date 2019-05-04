cbeba855b84753d4c93adc6d731debd35368b8c9
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-05-04 15:59:32 -0400

    styling

--- public_html/advising/form1.php
+++ public_html/advising/form1.php
@@ -327,7 +327,7 @@ background: linear-gradient(#ffdd7f 5%, #ffbc00 100%);
       }
 
       if ($form1===1) {
-
+echo $form1;
         //  echo '<br>Input data is successful';
           header("Location: viewform1.php");
 
