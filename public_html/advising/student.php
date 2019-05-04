45012a2cd97b3e5728ee8101da5392c696f534ec
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-05-03 22:14:58 -0400

    view form1 hopefully fixed?

--- public_html/advising/student.php
+++ public_html/advising/student.php
@@ -65,7 +65,7 @@ $db = new mysqli($servername, $username, $password, $dbname);
      }
 ?>
 
-      <a href="logout.php" class="w3-button  w3-block w3-hover-red w3-padding-16">Logout</a>
+      <a href="http://gwupyterhub.seas.gwu.edu/~lilyrschwarz/SJL/public_html/registration/menu.php" class="w3-button  w3-block w3-hover-red w3-padding-16">Main Menu</a>
 
 
     </div>
