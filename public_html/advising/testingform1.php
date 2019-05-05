19dbb280ec7b019b1132f8c44185be5c36548ceb
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-05-05 17:53:28 -0400

    testingform1

--- public_html/advising/testingform1.php
+++ public_html/advising/testingform1.php
@@ -62,153 +62,65 @@ $university_id = $_SESSION['uid'];
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <style>
-    /*tbody tr:nth-child(odd) {
-        background-color: #ff33cc;
-    }
+  ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
 
-    tbody tr:nth-child(even) {
-        background-color: #e495e4;
-    }
+li {
+  float: left;
+}
 
-    h2 {
-      color: #5689DF;
-    }*/
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-family: sans-serif;
 
-    .center{
-      text-align: center;
-    }
+}
 
-    .topright {
-        position: absolute;
-        right: 10px;
-        top: 20px;
-      }
-      .btn {
-        background-color: #990000;
-        color: white;
-        padding: 12px;
-        margin: 10px 0;
-        border: none;
-        width: 40%;
-        border-radius: 3px;
-        cursor: pointer;
-        font-size: 17px;
-      }
-      .field {
-        position: absolute;
-        left: 180px;
-      }
-      /*body{line-height: 1.6;}*/
-      .bottomCentered{
-         position: fixed;
-         text-align: center;
-         bottom: 30px;
-         width: 100%;
-      }
-      .error {color: #FF0000;}
-      .topright {
-        position: absolute;
-        right: 10px;
-        top: 10px;
-      }
+li a:hover:not(.active) {
+  background-color: #111;
+}
 
-      ul {
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-      overflow: hidden;
-      background-color: #333;
-      }
+.active {
+  background-color: #4CAF50;
+}
+</style>
+<ul>
+<li><a class="active" href="student.php">Home</a></li>
+<li><a href="StudentEnrollmentInfo.php">Current Enrolment</a></li>
+<li><a href="transcript.php">Transcript</a></li>
+<li><a href="studentinfo.php">Update Info</a></li>
+<li><a href="viewStudentPersonalInfo.php">View Info</a></li>
+<li><a href="form1.php">Update Form 1</a></li>
+<li><a href="viewform1.php">View Form 1</a></li>
+<li><a href="applytograduate.php">Apply to Graduate</a></li>
 
-      li {
-      float: left;
-      }
 
-      li a {
-      display: block;
-      color: white;
-      text-align: center;
-      padding: 14px 16px;
-      text-decoration: none;
-      }
+<?php
 
-      li a:hover:not(.active) {
-      background-color: #111;
-      }
+if (!empty($program_type)) {
+  //foreach($course_array as $key=>$value)
+  while($row = $program_type->fetch_assoc())
+  {
+    if($row['program_type'] == 'PhD'){
+?>
 
-      .active {
-        background-color: #990000;
-      }
-
-  </style>
-
-  <link rel="stylesheet" href="style.css">
-  <head>
-  <!--  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" type="text/css" href="style.css" />-->
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
-    <!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
-    <!-- <style>
-    ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #333;
-  }
-
-  li {
-    float: left;
-  }
-
-  li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-family: sans-serif;
-
-  }
-
-  li a:hover:not(.active) {
-    background-color: #111;
-  }
-
-  .active {
-    background-color: #4CAF50;
-  }
-  </style> -->
-  <ul>
-    <li><a class="active" href="student.php">Advising Home</a></li>
-    <!-- <li><a href="StudentEnrollmentInfo.php">Current Enrolment</a></li>
-    <li><a href="transcript.php">Transcript</a></li>
-    <li><a href="studentinfo.php">Update Info</a></li>
-    <li><a href="viewStudentPersonalInfo.php">View Info</a></li> -->
-    <!-- <li><a href="form1.php">Update Form 1</a></li>
-    <li><a href="viewform1.php">View Form 1</a></li> -->
-    <!-- <li><a href="applytograduate.php">Apply to Graduate</a></li> -->
-
-  <!--
-  <?php
-
-  if (!empty($program_type)) {
-    //foreach($course_array as $key=>$value)
-    while($row = $program_type->fetch_assoc())
-    {
-      if($row['program_type'] == 'PhD'){
-  ?>
-
-  <li><a href="submitThesisFile.php" >Submit Thesis</a><li>
-    <li><a href="<?php echo $url;?>" target="_blank">View Thesis</a><li>
-  <?php
-  }
-  }
-  }
-            ?> -->
-  <!-- <li><a href="http://gwupyterhub.seas.gwu.edu/~lilyrschwarz/SJL/public_html/registration/menu.php"  style="float:left">Main Menu</a></li> -->
-  <li style="float:right"><a href="logout.php"  >Logout</a></li></ul><br/></br>
-  <style>
+<li><a href="submitThesisFile.php" >Submit Thesis</a><li>
+  <li><a href="<?php echo $url;?>" target="_blank">View Thesis</a><li>
+<?php
+}
+}
+}
+          ?>
+<li><a href="logout.php">Logout</a></li>
+</ul><br/></br>
 <style>
 
 
