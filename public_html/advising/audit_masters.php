1654cf41922cd74caafe11becfe9e7bd5d4cf970
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-05-05 09:46:59 -0400

    homepage

--- public_html/advising/audit_masters.php
+++ public_html/advising/audit_masters.php
@@ -1,17 +1,15 @@
 <?php
-    session_start();
-    /*************************************************/
-    /* only let logged in users access, and protects */
-    /* this page so that only students can access.   */
-    /* else, redirects to login page                 */
-    /*************************************************/
-    if($_SESSION['uid'] && ($_SESSION['type'] == 'PHD' || $_SESSION['type'] == 'MS')){
+session_start();
 
-    }
-    else{
-        echo $_SESSION['uid'].$_SESSION['type'];
-        header("Location: http://gwupyterhub.seas.gwu.edu/~selingonal/SJL/public_html/advising/student.php");
-    }
+if($_SESSION['uid'] && $_SESSION['type'] == 'MS' || $_SESSION['type'] == 'PHD'){
+
+}
+else{
+    echo $_SESSION['uid'].$_SESSION['type'];
+    header("Location: http://gwupyterhub.seas.gwu.edu/~selingonal/SJL/public_html/registration/menu.php");
+}
+
+
 
     //connect to database
     $servername = "localhost";
@@ -21,6 +19,179 @@
     $db = new mysqli($servername, $username, $password, $dbname);
 
 
+
+    $program_type = $db->query("SELECT program_type from student where university_id =".$_SESSION['uid']);
+
+    $thesis_url = $db->query("SELECT FileName, FilePath from thesis where university_id =".$_SESSION['uid']);
+    while ($row2 = mysqli_fetch_array($thesis_url )) {
+    $url = $row2['FilePath'].$row2['FileName'];
+  //  var_dump($url);
+    }
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<!--  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link rel="stylesheet" type="text/css" href="style.css" />-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <style>
+    /*tbody tr:nth-child(odd) {
+        background-color: #ff33cc;
+    }
+
+    tbody tr:nth-child(even) {
+        background-color: #e495e4;
+    }
+
+    h2 {
+      color: #5689DF;
+    }*/
+
+    .center{
+      text-align: center;
+    }
+
+    .topright {
+        position: absolute;
+        right: 10px;
+        top: 20px;
+      }
+      .btn {
+        background-color: #990000;
+        color: white;
+        padding: 12px;
+        margin: 10px 0;
+        border: none;
+        width: 40%;
+        border-radius: 3px;
+        cursor: pointer;
+        font-size: 17px;
+      }
+      .field {
+        position: absolute;
+        left: 180px;
+      }
+      /*body{line-height: 1.6;}*/
+      .bottomCentered{
+         position: fixed;
+         text-align: center;
+         bottom: 30px;
+         width: 100%;
+      }
+      .error {color: #FF0000;}
+      .topright {
+        position: absolute;
+        right: 10px;
+        top: 10px;
+      }
+
+      ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background-color: #333;
+      }
+
+      li {
+      float: left;
+      }
+
+      li a {
+      display: block;
+      color: white;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+      }
+
+      li a:hover:not(.active) {
+      background-color: #111;
+      }
+
+      .active {
+        background-color: #990000;
+      }
+
+  </style>
+
+  <link rel="stylesheet" href="style.css">
+  <head>
+  <title>View Info</title>
+  <!-- <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
+  <!-- <style>
+  ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  }
+
+  li {
+  float: left;
+  }
+
+  li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-family: sans-serif;
+
+  }
+
+  li a:hover:not(.active) {
+  background-color: #111;
+  }
+
+  .active {
+  background-color: #4CAF50;
+  }
+  </style> -->
+  </head>
+  <body>
+    <ul>
+    <li><a class="active" href="student.php">Advising Home</a></li>
+    <!-- <li><a href="StudentEnrollmentInfo.php">Current Enrolment</a></li>
+    <li><a href="transcript.php">Transcript</a></li>
+    <li><a href="studentinfo.php">Update Info</a></li>
+    <li><a href="viewStudentPersonalInfo.php">View Info</a></li> -->
+    <!-- <li><a href="form1.php">Update Form 1</a></li>
+    <li><a href="viewform1.php">View Form 1</a></li>
+    <li><a href="applytograduate.php">Apply to Graduate</a></li> -->
+
+
+    <!-- <?php
+
+    if (!empty($program_type)) {
+      //foreach($course_array as $key=>$value)
+      while($row = $program_type->fetch_assoc())
+      {
+        if($row['program_type'] == 'PhD'){
+    ?>
+
+    <li><a href="submitThesisFile.php" >Submit Thesis</a><li>
+   <li><a href="<?php echo $url;?>" target="_blank">View Thesis</a><li>
+  <?php
+    }
+   }
+  }
+              ?> -->
+    <!-- <li><a href="http://gwupyterhub.seas.gwu.edu/~lilyrschwarz/SJL/public_html/registration/menu.php">Main Menu</a></li> -->
+    <li style="float:right"><a href="logout.php">Logout</a></li>
+
+  </ul><br/></br>
+</ul><br/></br>
+<div align="center">
+  <h2>Eligibility:</h2>
+
+    <?php
     /*****************************************************/
     /* This value starts as 1, but if any of the checks  */
     /* to graduate fail, it gets changed to 0 which will */
@@ -127,8 +298,8 @@
         }
     }
     else{
-	    echo "Could not access GPA information. Returning... <br />";
-	    echo $db->error;
+      echo "Could not access GPA information. Returning... <br />";
+      echo $db->error;
         $cleared = 0;
     }
 
@@ -192,4 +363,10 @@
         echo "Cleared for graduation!";
     }
 
-?>
+
+
+    ?>
+
+  </div>
+</body>
+</html>
