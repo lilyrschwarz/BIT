c416d458c67ca678c8a82e1ec1dc2a44f705a498
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-05-05 19:10:54 -0400

    FORM 1 WORKS

--- public_html/advising/audit_masters.php
+++ public_html/advising/audit_masters.php
@@ -307,9 +307,6 @@ else{
     /* FIFTH CHECK: overall credits > 30               */
     /***************************************************/
 
-    $credits_sum = $db->query("SELECT sum(c.credits) as sum_of_credits from course c, transcript t where '".$_SESSION['uid']."'=t.uid AND t.crn=c.crn");
-    $credits_sum = $credits_sum->fetch_assoc();
-    $credits_sum = $credits_sum['sum_of_credits'];
 
     // $sql = "SELECT total_credits FROM student WHERE university_id = '.$user.';";
     // $result_5 = mysqli_query($db,$sql);
