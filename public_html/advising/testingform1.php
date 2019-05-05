df1842192f6598ce958008cb6d37d1ae879c56b3
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-05-05 18:19:01 -0400

    FORM 1 WORKS

--- public_html/advising/testingform1.php
+++ public_html/advising/testingform1.php
@@ -60,38 +60,89 @@ $university_id = $_SESSION['uid'];
 <!--  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <link rel="stylesheet" type="text/css" href="style.css" />-->
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="style.css">
   <style>
-  ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
-}
+    /*tbody tr:nth-child(odd) {
+        background-color: #ff33cc;
+    }
 
-li {
-  float: left;
-}
+    tbody tr:nth-child(even) {
+        background-color: #e495e4;
+    }
 
-li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-family: sans-serif;
+    h2 {
+      color: #5689DF;
+    }*/
 
-}
+    .center{
+      text-align: center;
+    }
 
-li a:hover:not(.active) {
-  background-color: #111;
-}
+    .topright {
+        position: absolute;
+        right: 10px;
+        top: 20px;
+      }
+      .btn {
+        background-color: #990000;
+        color: white;
+        padding: 12px;
+        margin: 10px 0;
+        border: none;
+        width: 40%;
+        border-radius: 3px;
+        cursor: pointer;
+        font-size: 17px;
+      }
+      .field {
+        position: absolute;
+        left: 180px;
+      }
+      /*body{line-height: 1.6;}*/
+      .bottomCentered{
+         position: fixed;
+         text-align: center;
+         bottom: 30px;
+         width: 100%;
+      }
+      .error {color: #FF0000;}
+      .topright {
+        position: absolute;
+        right: 10px;
+        top: 10px;
+      }
 
-.active {
-  background-color: #4CAF50;
-}
-</style>
+      ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background-color: #333;
+      }
+
+      li {
+      float: left;
+      }
+
+      li a {
+      display: block;
+      color: white;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+      }
+
+      li a:hover:not(.active) {
+      background-color: #111;
+      }
+
+      .active {
+        background-color: #990000;
+      }
+
+  </style>
+
+  <link rel="stylesheet" href="style.css">
 <ul>
 <li><a class="active" href="student.php">Home</a></li>
 <li><a href="StudentEnrollmentInfo.php">Current Enrolment</a></li>
