f33c3f0a7547d198bcb20f498f2007454f0e19b0
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-05-06 09:26:37 -0400

    research

--- public_html/advising/audit_phd.php
+++ public_html/advising/audit_phd.php
@@ -298,7 +298,7 @@ else{
     $sql = "SELECT thesis_approved FROM student WHERE university_id = '.$user.';";
     $result_6 = mysqli_query($db,$sql);
     if(!empty($result_6)){
-        if($result_6 === 1){
+        if($sql === 1){
             echo "Thesis Has Been Approved by the GS!<br/>";
 
         }else{
